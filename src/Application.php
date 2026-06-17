c80a4e03d8f1a8ba294fc5fda54721f249ac853d
USAMI Kenta <tadsan@zonu.me>
2026-06-17 16:30:48 +0900

    Add PSR-4 autoloading and Satiate\Application class

---
+++ src/Application.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Satiate;
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+
+final readonly class Application extends SymfonyApplication
+{
+    public const string NAME = 'Satiate';
+    public const string VERSION = '0.1.0-dev';
+
+    public function __construct()
+    {
+        parent::__construct(self::NAME, self::VERSION);
+    }
+}
